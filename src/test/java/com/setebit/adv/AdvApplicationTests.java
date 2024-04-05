af30bf407e144941696ba2e81871d952df4eb4fc
Claudemir <claudemirramosferreira@gmail.com>
2024-04-05 17:20:05 -0300

    commit initial

---
+++ src/test/java/com/setebit/adv/AdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setebit.adv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
